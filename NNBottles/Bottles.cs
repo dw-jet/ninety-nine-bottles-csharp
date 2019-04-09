06c87cb30481f413722f4d3af000d712fe0726b1
James Teitsworth <james.teitsworth@outlook.com>
2019-04-08 22:22:32 -0500

    Use flocking to find an abstraction that removes case 2
    Small step by step iterative changes with test runs on each change allows us to factor out the need for case 2 by identifying the difference that created the case and factoring it out into a new method.

--- NNBottles/Bottles.cs
+++ NNBottles/Bottles.cs
@@ -4,6 +4,10 @@ namespace NNBottles
 {
     public static class Bottles
     {
+        private static string Container(int number)
+        {
+            return number == 1 ? "bottle" : "bottles";
+        }
         public static string Verse(int verseNumber)
         {
             switch (verseNumber)
@@ -18,15 +22,10 @@ namespace NNBottles
                     return "1 bottle of beer on the wall, 1 bottle of beer." +
                            "\nTake it down and pass it around, no more bottles of beer on the wall.\n";
                 }
-                case 2:
-                {
-                    return $"{verseNumber} bottles of beer on the wall, {verseNumber} bottles of beer." +
-                           $"\nTake one down and pass it around, {verseNumber - 1} bottle of beer on the wall.\n";
-                }
                 default:
                 {
                     return $"{verseNumber} bottles of beer on the wall, {verseNumber} bottles of beer." +
-                           $"\nTake one down and pass it around, {verseNumber - 1} bottles of beer on the wall.\n";
+                           $"\nTake one down and pass it around, {verseNumber - 1} {Container(verseNumber - 1)} of beer on the wall.\n";
                 }
             }
         }
