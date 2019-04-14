19f5653b21b79dddb871cef46ba2f0785048d4a1
James Teitsworth <james.teitsworth@outlook.com>
2019-04-13 20:07:45 -0500

    Remove differences in the beginning of 0 and else cases
    The beginning of the 0 case should read no more which quantity already does, but it's not capitalized. I created a helper method to capitalize the first letter and used that to make the beginnings identical.

--- NNBottles/Bottles.cs
+++ NNBottles/Bottles.cs
@@ -18,18 +18,31 @@ namespace NNBottles
         {
             return number == 0 ? "no more" : number.ToString();
         }
+
+        private static string Capitalize(string str)
+        {
+            // Capitalize the first letter of a string
+            // in an efficient way
+            if (string.IsNullOrEmpty(str))
+            {
+                return string.Empty;
+            }
+            char[] charArray = str.ToCharArray();
+            charArray[0] = char.ToUpper(charArray[0]);
+            return new string(charArray);
+        }
         public static string Verse(int number)
         {
             switch (number)
             {
                 case 0:
                 {
-                    return "No more bottles of beer on the wall, no more bottles of beer." +
+                    return $"{Capitalize(Quantity(number))} bottles of beer on the wall, no more bottles of beer." +
                            "\nGo to the store and buy some more, 99 bottles of beer on the wall.\n";
                 }
                 default:
                 {
-                    return $"{number} {Container(number)} of beer on the wall, {number} {Container(number)} of beer." +
+                    return $"{Capitalize(Quantity(number))} {Container(number)} of beer on the wall, {number} {Container(number)} of beer." +
                            $"\nTake {Pronoun(number)} down and pass it around, {Quantity(number - 1)} {Container(number - 1)} of beer on the wall.\n";
                 }
             }
